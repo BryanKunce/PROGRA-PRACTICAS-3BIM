545bd8ed43b89900d7a8247b14ca96d32c83babe
Bryan Kunce <129023899+BryanKunce@users.noreply.github.com>
2023-05-30 09:33:19 -0600

    Add files via upload

---
+++ sketch_may30a02.ino
@@ -0,0 +1,98 @@
+                                                                  /*BRYAN KUNCE - 2022096 - EB5BM - PRACTICA 1 - TERCER BIMESTRE*/
+
+
+#include <LiquidCrystal_I2C.h>//Incluimos la libreria para el LCD con el I2C
+
+#define lcdAddress 0x27//Definimos las filas y columas del LCD
+#define filas 2
+#define columnas 16
+LiquidCrystal_I2C lcd (0x27, 2, 16);
+
+#define buzz 7//Definimos que el pin del buzzer sera una salida y cuando este on/off
+#define outbuzz() pinMode(buzz,OUTPUT)
+#define buzzon() tone(buzz, 958)
+#define buzzoff() noTone(buzz)
+
+#define bom 22//Definimos el pin de la bomba de agua y los estados de HIGH/LOW
+#define outbom() pinMode(bom, OUTPUT)
+#define onbom() digitalWrite (bom, HIGH)
+#define offbom() digitalWrite (bom, LOW)
+
+#define Trig 24//Definimos pines para el sensor ultrasonico IN/OUT HIGH/LOW
+#define echo 25
+#define onsonic() digitalWrite(Trig,HIGH)
+#define offsonic() digitalWrite(Trig,LOW)
+#define outsonic() pinMode(Trig,OUTPUT)
+#define insonic() pinMode(Trig,INPUT)
+
+#define velocidad 0.0343
+
+
+
+
+
+
+void setup() {
+ lcd.init();//Inicializar el lcd
+ lcd.backlight();//Activar el led del lcd
+ outbuzz();//Salida del buzzer
+
+ outsonic();//Definimos como salida el pin del trig
+ insonic();//Definimos como entrada el pin de echo
+ Serial.begin(9600);//Inicializamos el monitor serial
+ attachInterrupt(digitalPinToInterrupt(2),cambio,LOW);
+
+}
+
+void loop() {
+ 
+
+
+  onsonic();
+  delay(1);
+  offsonic();
+  float time = pulseIn(echo,HIGH);
+  float distancia = (time*velocidad)/2;
+
+  lcd.setCursor(0,1);//Colocamos en que fila y que clumna queremos que este el cursor  
+  lcd.print("DISTANCIA");
+  
+  Serial.println(distancia);
+
+ if(distancia > 200){
+  lcd.print("**");
+  }
+
+  if(distancia > 300){
+  lcd.print("****");
+  }
+
+  if(distancia > 200){
+  lcd.print("******");
+  }
+
+  if(distancia > 200){
+  lcd.print("********");
+  }
+
+  if(distancia > 200){
+  lcd.print("**********");
+  }
+
+  if(distancia > 200){
+  lcd.print("************");
+  }
+
+  if(distancia > 200){
+  lcd.print("**************");
+  }
+
+  if(distancia > 200){
+  lcd.print("**************");
+  }
+
+}
+
+void cambio(){
+Serial.println("Interrupcion");
+  }
